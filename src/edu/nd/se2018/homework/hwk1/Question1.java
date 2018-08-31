b699e0e36d119bdd58fe44289cb69b69661ed7c7
Michael Wang <MichaelWang@michaels-mbp.dhcp.nd.edu>
2018-08-31 14:29:09 -0400

    test push from local

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,23 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		Set<Integer> seen = new HashSet<Integer>();
+		int total = 0;
+		for (int i : numbers) {
+			if (seen.contains(i))
+				continue;
+			else {
+				total += i;
+				seen.add(i);
+			}		
+		}
+		return total;	
 	}
 }
