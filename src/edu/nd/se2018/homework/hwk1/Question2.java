545b4650b3e1d3d2194859850724f8de47454da3
Michael Wang <MichaelWang@michaels-mbp.dhcp.nd.edu>
2018-08-31 14:30:40 -0400

    homework1 finished

--- src/edu/nd/se2018/homework/hwk1/Question2.java
+++ src/edu/nd/se2018/homework/hwk1/Question2.java
@@ -1,10 +1,47 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Question2 {
 
 	public Question2(){}
 	
 	public String getMostFrequentWord(String input, String stopwords){
-		return "";
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		Set<String> stopWords = new HashSet<String>();
+		String[] inputArr = input.split("\\s+");
+		String[] stopwordsArr = stopwords.split("\\s+");
+		String frequentWord = "";
+		int maxcount = 0;
+		int maximum = 0;
+		
+		for (String sw : stopwordsArr)
+			stopWords.add(sw);
+		for (String word : inputArr) {
+			if (!Arrays.asList(stopwordsArr).contains(word)) {
+				if (m.get(word) == null)
+					m.put(word, 1);
+				else
+					m.put(word, m.get(word) + 1);
+			}
+		}
+		if (m.isEmpty())
+			return null;
+		maximum = Collections.max(m.values());
+		for (Entry<String, Integer> entry : m.entrySet()) {
+			if (entry.getValue() == maximum) {
+				maxcount += 1;
+				frequentWord = entry.getKey();
+				if (maxcount > 1)
+					return null;
+			}
+		}
+		return frequentWord;
 	}
 }
--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -1,9 +1,33 @@
 package edu.nd.se2018.homework.hwk1;
+
 public class Question3 {
 	
 	public Question3(){}	
 	
     public int getMirrorCount(int[] numbers){
-		return 0;	
+    	if (numbers.length <= 1)
+    		return numbers.length;
+
+    	int start = 0;
+    	int kernel = numbers.length + 1;
+    	StringBuilder builder = new StringBuilder();
+    	for (int i : numbers) {
+    		builder.append(i);
+    	}
+    	String strNumbers = builder.toString();
+    	while (kernel != 0) {
+    		int end = start + kernel - 1;
+    		while (end != numbers.length + 1) {
+    			StringBuilder sb = new StringBuilder(strNumbers);
+    			if (strNumbers.substring(start, end).equals(sb.reverse().substring(start,end)))
+    				return strNumbers.substring(start, end).length();
+    		
+    			start += 1;
+    			end += 1;
+    		}
+    		start = 0;
+    		kernel -= 1;
+    	}
+		return 1;
 	}
 }
